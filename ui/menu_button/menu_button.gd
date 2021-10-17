ea5969cdadeee0c19b1001e3a8095e87d388ccc7
Joe <joe.zlonicky@gmail.com>
2021-10-16 21:55:32 -0700

    Improved main menu

--- ui/menu_button/menu_button.gd
+++ ui/menu_button/menu_button.gd
@@ -7,3 +7,24 @@ func _on_Button_mouse_entered():
 
 func _on_Button_mouse_exited():
 	$AnimationPlayer.play("default")
+
+
+func _on_Button_button_down():
+	$AnimationPlayer.play("button_down")
+
+
+func _on_Button_button_up():
+	$AnimationPlayer.play("button_up")
+	$AnimationPlayer.queue("mouse_entered")
+
+
+func _on_BeginButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_SettingsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
