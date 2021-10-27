cb395ba4d7bb24d5cb69e65e99ef0195d7388e50
Joe <joe.zlonicky@gmail.com>
2021-10-26 21:17:13 -0700

    Flea jump animation

--- flea/flea.gd
+++ flea/flea.gd
@@ -13,9 +13,9 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_pressed("move_right"):
 			velocity = Vector2(speed, -jump_force)
+			$AnimationPlayer.play("jump")
 		else:
 			velocity = Vector2(0, 0)
 	
 	velocity.y = min(velocity.y + gravity * delta, terminal_velocity)
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
