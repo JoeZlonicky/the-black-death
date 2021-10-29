38a4edd3956c70d7a3b9e3bcbebf6de0b4d4fae1
JoeZlonicky <joe.zlonicky@gmail.com>
2021-10-29 10:07:45 -0700

    Polish forest narrative

--- narratives/forest/forest_narrative.gd
+++ narratives/forest/forest_narrative.gd
@@ -1,5 +1,6 @@
 extends "res://narratives/narrative.gd"
 
+
 func _on_End_body_entered(body):
 	if body.is_in_group("marmot"):
 		body.kill()
@@ -7,3 +8,8 @@ func _on_End_body_entered(body):
 
 func _on_Marmot_killed():
 	$Overlay/Fade/AnimationPlayer.play("fade_out")
+
+
+func next_scene():
+	var _ret = get_tree().change_scene("res://narratives/travelling/travelling_narrative.tscn")
+	print("Changed scene")
